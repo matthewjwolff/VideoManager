2bd7687ff5ec4a65085674de1b77d3747d05c3e1
Xin <Xin@XS-PC>
2015-04-15 20:03:33 -0500

    Main: added random test tags

--- src/videomanager/VideoManagerMain.java
+++ src/videomanager/VideoManagerMain.java
@@ -5,12 +5,24 @@
  */
 package videomanager;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author Matthew Wolff
  */
 public class VideoManagerMain {
-
+    
+    public static String[] charArray = {"Captain Falcon", "Donkey Kong", "Fox", 
+        "Jigglypuff", "Kirby", "Link", "Luigi", "Mario", "Ness", "Pikachu",
+        "Samus", "Yoshi, Bowser", "Dr. Mario", "Falco", "Ganondorf", 
+        "Ice Climbers", "Marth", "Mewtwo", "Mr. Game & Watch", "Peach", 
+        "Pichu", "Roy", "Sheik", "Young Link", "Zelda"};
+    public static String[] stageArray = {"Final Destination", "Battlefield",
+        "Dreamland", "Yoshi's Story", "Fountain of Dreams", "Pokemon Stadium",
+        "Kongo Jungle"};
+    
     /**
      * @param args the command line arguments
      */
@@ -18,6 +30,22 @@ public class VideoManagerMain {
     {
         //For Quin: to launch gui, uncomment following line.
         //videomanager.gui.VideoManagerClient.main(args);
+        
+        Library videoLib = new Library(fileLoc)
+        
+        ArrayList<Tag> testTags = new ArrayList<>();
+        Random r = new Random();
+        
+        //Get random character:
+        int charIdx = r.nextInt(charArray.length);
+        testTags.add(new Tag("character",charArray[charIdx]));
+        
+        //Get random stage:
+        int stageIdx = r.nextInt(stageArray.length);
+        testTags.add(new Tag("stage",charArray[stageIdx]));
+        
+        ArrayList<Video> resultVideos = new QueryResult(videoLib, testTags).getResult();
+        
     }
     
 }
