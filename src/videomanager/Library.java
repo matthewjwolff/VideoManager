54a15866c59c4c70f6af17837319a47431560c60
Matthew Wolff <mwolff3@tigers.lsu.edu>
2015-04-15 19:57:56 -0500

    I think this completes XML saving...

--- src/videomanager/Library.java
+++ src/videomanager/Library.java
@@ -6,14 +6,21 @@
 package videomanager;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -24,13 +31,54 @@ import org.xml.sax.SAXException;
 public class Library extends ArrayList<Video> {
     
     //Make an XML and save the library
-    public void saveTo(File location) throws ParserConfigurationException
+    /**
+     * Saves the current Library as an XML File formatted thusly:
+     * xml
+     *      video
+     *          location ... /location
+     *          title ... /title
+     *          tag1 ... /tag1
+     *          ...
+     *      /video
+     * /xml
+     * @param file to save to
+     * @throws ParserConfigurationException
+     * @throws TransformerConfigurationException
+     * @throws TransformerException
+     * @throws FileNotFoundException
+     * @throws IOException 
+     */
+    public void saveTo(File file) throws ParserConfigurationException, TransformerConfigurationException, TransformerException, FileNotFoundException, IOException
     {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.newDocument();
+        Node currentNode = doc.appendChild(doc.createElement("xml"));
+        for(Video v : this)
+        {
+            currentNode = currentNode.appendChild(doc.createElement("video"));
+            currentNode.appendChild(doc.createElement("location").appendChild(doc.createTextNode(v.location)));
+            currentNode.appendChild(doc.createElement("title").appendChild(doc.createTextNode(v.title)));
+            for(Tag t : v.tags)
+                currentNode.appendChild(doc.createElement(t.type).appendChild(doc.createTextNode(t.value)));
+        }
+        TransformerFactory tFactory = TransformerFactory.newInstance();
+        Transformer transformer = tFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+        try (FileOutputStream output = new FileOutputStream(file)) {
+            StreamResult result = new StreamResult(output);
+            transformer.transform(source, result);
+        }
     }
     
     //load from xml
+    /**
+     * Loads the previous run's library
+     * @param file file to load from
+     * @throws SAXException
+     * @throws ParserConfigurationException
+     * @throws IOException 
+     */
     public Library(File file) throws SAXException, ParserConfigurationException, IOException
     {
         super();
