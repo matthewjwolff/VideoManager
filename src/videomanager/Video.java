823fb08e5007ca27d77d4e46fc06d27e6c127ed6
Matthew Wolff <mwolff3@tigers.lsu.edu>
2015-04-16 00:38:55 -0500

    Two Major Changes:
    1. Video's location is now a URI. This is a Java class that models a Universal Resource Identifier (more general version of URL). This allows for easily opening the video via the watch() method.
    
    2. Changed nearly all ArrayLists to HashSets to prevent duplicate entries. Shouldn't make much of a difference in code. Both can be iterated in a for each loop as such
    
    for(Element e : ArrayList/HashSet set)
    {
        code here;
    }
    
    I don't see any reason why we'd need to access a specific element in a set without searching through the whole thing. If you need help Xin, look up Oracle's documentation on HashSet.

--- src/videomanager/Video.java
+++ src/videomanager/Video.java
@@ -5,17 +5,23 @@
  */
 package videomanager;
 
-import java.util.ArrayList;
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Matthew Wolff
  */
 public class Video {
-    public String location;
+    public URI location;
     public Time length;
     public String title;
-    public ArrayList<Tag> tags;
+    public HashSet<Tag> tags;
     public int hash;
     
     /**
@@ -24,11 +30,46 @@ public class Video {
      * @param title Name of video
      * @param tags the tags associated with this video
      */
-    public Video(String location, String title, ArrayList<Tag> tags)
+    public Video(URI location, String title, HashSet<Tag> tags)
     {
         this.location = location;
         this.tags = tags;
         this.title = title;
+    }
+    
+    /**
+     * Returns true if this video points to the same location as another.
+     * Possibly check for URI's .equals()
+     * This method only takes the location (a URI) into account.
+     * @param o the object to compare
+     * @return true (possibly) if both locations are the same, false if they are different or o is not a Video.
+     */
+    @Override
+    public boolean equals(Object o)
+    {
+        if(o instanceof Video)
+            return ((Video)o).location.equals(this.location);
+        else return false;
+    }
+
+    /**
+     * Auto generated hash code by Netbeans taking only location into account
+     * @return the hash code of this object.
+     */
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + Objects.hashCode(this.location);
+        return hash;
+    }
+    
+    public void watch()
+    {
+        try {
+            Desktop.getDesktop().browse(location);
+        } catch (IOException ex) {
+            Logger.getLogger(Video.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
