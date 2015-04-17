4237e5d10420a04d726cd1315ed6245719cf956b
Matthew Wolff <mwolff3@tigers.lsu.edu>
2015-04-17 15:10:35 -0500

    Added Quin's GUI

--- src/videomanager/gui/VideoManagerClient.java
+++ src/videomanager/gui/VideoManagerClient.java
@@ -27,11 +27,125 @@ public class VideoManagerClient extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        panel1 = new java.awt.Panel();
+        CharacterAdd = new javax.swing.JComboBox();
+        CharacterAddButton = new javax.swing.JButton();
+        StageAddButton = new javax.swing.JButton();
+        StageAdd = new javax.swing.JComboBox();
+        PlayerTag = new javax.swing.JTextField();
+        CharacterLabel = new javax.swing.JLabel();
+        StageLabel = new javax.swing.JLabel();
+        PlayerLabel = new javax.swing.JLabel();
+        PlayerAddButton = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        VideoUrlLabel = new javax.swing.JLabel();
+        VideoUrl = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        CharacterTagLabel = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenu2 = new javax.swing.JMenu();
 
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout panel1Layout = new javax.swing.GroupLayout(panel1);
+        panel1.setLayout(panel1Layout);
+        panel1Layout.setHorizontalGroup(
+            panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        panel1Layout.setVerticalGroup(
+            panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        CharacterAdd.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Bowser", "Captain Falcon", "Donkey Kong", "Dr. Mario", "Falco", "Fox", "Gannondorf", "Ice Climbers", "Jigglypuff", "Kirby", "Luigi", "Link", "Mario", "Marth", "Mewtwo", "Mr.Game&Watch", "Ness", "Peach", "Pichu", "Pikachu", "Roy", "Samus", "Sheik", "Yoshi", "Young Link", "Zelda" }));
+        CharacterAdd.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                CharacterAddActionPerformed(evt);
+            }
+        });
+
+        CharacterAddButton.setText("Add");
+        CharacterAddButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                CharacterAddButtonActionPerformed(evt);
+            }
+        });
+
+        StageAddButton.setText("Add");
+        StageAddButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                StageAddButtonActionPerformed(evt);
+            }
+        });
+
+        StageAdd.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Battlefield", "Dreamland", "Final Destination", "Fountain of Dreams", "Pokemon Stadium", "Yoshi's Story" }));
+        StageAdd.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                StageAddActionPerformed(evt);
+            }
+        });
+
+        PlayerTag.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                PlayerTagActionPerformed(evt);
+            }
+        });
+
+        CharacterLabel.setText("Characters");
+
+        StageLabel.setText("Stages");
+
+        PlayerLabel.setText("Players");
+
+        PlayerAddButton.setText("Add");
+        PlayerAddButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                PlayerAddButtonActionPerformed(evt);
+            }
+        });
+
+        jButton4.setText("Clear");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        jButton5.setText("Accept");
+
+        VideoUrlLabel.setText("Video URL");
 
         jMenu1.setText("File");
         jMenuBar1.add(jMenu1);
@@ -45,15 +159,126 @@ public class VideoManagerClient extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(CharacterLabel)
+                    .addComponent(PlayerLabel)
+                    .addComponent(VideoUrlLabel)
+                    .addComponent(VideoUrl, javax.swing.GroupLayout.PREFERRED_SIZE, 197, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(CharacterAdd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(CharacterAddButton))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(StageAdd, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(StageAddButton))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(PlayerTag, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jButton4)
+                                    .addComponent(PlayerAddButton)))
+                            .addComponent(StageLabel))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel6)
+                                    .addComponent(jLabel5)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(jLabel4)
+                                        .addGap(68, 68, 68)
+                                        .addComponent(jLabel1))
+                                    .addComponent(jButton5)))
+                            .addComponent(CharacterTagLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(VideoUrlLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(VideoUrl, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(CharacterLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(CharacterTagLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(CharacterAdd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(CharacterAddButton)
+                        .addComponent(jLabel4)))
+                .addGap(6, 6, 6)
+                .addComponent(StageLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(StageAddButton)
+                        .addComponent(jLabel5)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(StageAdd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(PlayerLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(PlayerTag, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(PlayerAddButton)
+                    .addComponent(jLabel6)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton4)
+                    .addComponent(jButton5))
+                .addGap(28, 28, 28))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void StageAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_StageAddActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_StageAddActionPerformed
+
+    private void PlayerTagActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_PlayerTagActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_PlayerTagActionPerformed
+
+    private void PlayerAddButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_PlayerAddButtonActionPerformed
+        String tag = PlayerTag.getText();
+        jLabel3.setText(jLabel3.getText() + ' ' + tag);
+        
+    }//GEN-LAST:event_PlayerAddButtonActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void CharacterAddButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CharacterAddButtonActionPerformed
+        String tag = CharacterAdd.getSelectedItem().toString();
+        String CharacterTagsText = CharacterTagLabel.getText() + ", " + tag;
+        CharacterTagLabel.setText(CharacterTagsText);
+    }//GEN-LAST:event_CharacterAddButtonActionPerformed
+
+    private void StageAddButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_StageAddButtonActionPerformed
+        String tag = StageAdd.getSelectedItem().toString();
+        jLabel2.setText(jLabel2.getText() + ' ' + tag);
+    }//GEN-LAST:event_StageAddButtonActionPerformed
+
+    private void CharacterAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CharacterAddActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_CharacterAddActionPerformed
 
     /**
      * @param args the command line arguments
@@ -91,8 +316,31 @@ public class VideoManagerClient extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox CharacterAdd;
+    private javax.swing.JButton CharacterAddButton;
+    private javax.swing.JLabel CharacterLabel;
+    private javax.swing.JLabel CharacterTagLabel;
+    private javax.swing.JButton PlayerAddButton;
+    private javax.swing.JLabel PlayerLabel;
+    private javax.swing.JTextField PlayerTag;
+    private javax.swing.JComboBox StageAdd;
+    private javax.swing.JButton StageAddButton;
+    private javax.swing.JLabel StageLabel;
+    private javax.swing.JTextField VideoUrl;
+    private javax.swing.JLabel VideoUrlLabel;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private java.awt.Panel panel1;
     // End of variables declaration//GEN-END:variables
 }
